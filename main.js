eb13be078f03d8fe9ea6a25a385660b6ad926adc
MarllonDuartt <151869978+MarllonDuartt@users.noreply.github.com>
2023-11-24 06:13:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function tocaSomHybridTheory(){
+
+    document.querySelector("#som_tecla_HT").play();
+    }
+    document.querySelector(".tecla_HT").onclick = tocaSomHybridTheory;
+
+    function tocaSomMeteora(){
+
+        document.querySelector("#som_tecla_MT").play();
+        }
+        document.querySelector(".tecla_MT").onclick = tocaSomMeteora;
+
+        function tocaSomMinutestoMidnigth(){
+
+            document.querySelector("#som_tecla_MTM").play();
+            }
+            document.querySelector(".tecla_MTM").onclick = tocaSomMinutestoMidnigth; 
+            
+            function tocaSomAThousandSuns(){
+
+                document.querySelector("#som_tecla_ATS").play();
+                }
+                document.querySelector(".tecla_ATS").onclick = tocaSomAThousandSuns;    
